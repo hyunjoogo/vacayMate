3e862ba91e87e3bb141e9c3623b11d913ecc9fd1
HyunJoo Kim <hyunjoogo@gmail.com>
2023-03-29 00:07:13 +0900

    B - 로그인 API 개발완료

--- server/app.js
+++ server/app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require('cors');
 
 require('dotenv').config();
 require('./helpers/init_mongodb');
 
 const RegisterRoute = require('./routes/register');
+const LoginRoute = require('./routes/login');
 
 const app = express();
 app.use(morgan('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
@@ -15,7 +18,16 @@ app.get('/', async (req, res) => {
   res.send('Hello from express');
 });
 
+app.use('/login', LoginRoute);
 app.use('/register', RegisterRoute)
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    status: err.status || 500,
+    message: err.message,
+  });
+});
 
 const PORT = 3300;
 app.listen(PORT, () => {
--- server/routes/register.js
+++ server/routes/register.js
@@ -6,15 +6,21 @@ const jwt = require("jsonwebtoken");
 
 
 // 구글 OAuth2.0을 이용한 사용자 등록 API
-router.post('/google', async (req, res, next) => {
+router.post('/', async (req, res, next) => {
+  // ----------------------------- 임시 컬렉션 삭제
+  User.collection.drop();
+  //  -------------------------- 임시 컬렉션 삭제
+
   const oAuth2Client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_PASSWORD);
   const authHeader = req.headers.authorization;
   if (!authHeader) {
     next();
     return;
   }
-  const token = authHeader.split(' ')[1];
+
+
   try {
+    const token = authHeader.split(' ')[1];
     const ticket = await oAuth2Client.verifyIdToken({
       idToken: token,
       audience: process.env.GOOGLE_CLIENT_ID,
@@ -31,49 +37,14 @@ router.post('/google', async (req, res, next) => {
       });
       await user.save();
     }
-    const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {
+    const accessToken = jwt.sign({id: user._id}, process.env.JWT_SECRET, {
       expiresIn: '1h',
     });
-    res.status(200).json({token: token});
+    res.status(200).json({accessToken});
   } catch (error) {
     console.error(error);
     res.status(401).send('Google OAuth2.0 인증에 실패했습니다.');
   }
 });
-
-// email로 사용자 등록
-router.post('/email', async (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  const name = req.body.name;
-
-  // email과 password가 존재하지 않으면 400 에러 반환
-  if (!email || !password || !name) {
-    return res.status(400).send({error: 'Email and password are required.'});
-  }
-
-  try {
-    // email이 이미 존재하는지 확인
-    const existingUser = await User.findOne({email: email});
-    if (existingUser) {
-      return res.status(400).send({error: 'Email is already in use.'});
-    }
-
-    // 새로운 사용자 생성
-    const user = new User({email, password, name});
-    // TODO 비밀번호 해싱해서 저장하기
-    await user.save();
-
-    // JWT 토큰 생성
-    const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET);
-
-    // 응답 반환
-    res.send({user: user, token: token});
-  } catch (err) {
-    console.log(err);
-    res.status(500).send({error: 'Server error occurred.'});
-  }
-});
-
 
 module.exports = router;
