08d8f7bf54f0c771cadfc5a0922670cfd0650461
HyunJoo Kim <hyunjoogo@gmail.com>
2023-04-04 08:42:04 +0900

    B - 사용자 휴가사용요청 상세조회

--- server/app.js
+++ server/app.js
@@ -1,35 +1,37 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require('cors');
+const cors = require("cors");
 
-require('dotenv').config();
-require('./helpers/init_mongodb');
+require("dotenv").config();
+require("./helpers/init_mongodb");
 
-const RegisterRoute = require('./routes/register');
-const LoginRoute = require('./routes/login');
-const VacationRoute = require('./routes/vacation');
-const RequestRoute = require('./routes/request');
+const RegisterRoute = require("./routes/register");
+const LoginRoute = require("./routes/login");
+const VacationRoute = require("./routes/vacation");
+const RequestRoute = require("./routes/request");
+
+const verifyToken = require("./middlewares/verifyToken");
 
 const app = express();
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get('/', async (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", async (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.use('/login', LoginRoute);
-app.use('/register', RegisterRoute)
-app.use('/vacation', VacationRoute)
-app.use('/request', RequestRoute)
+app.use("/login", LoginRoute);
+app.use("/register", RegisterRoute);
+app.use("/vacation", VacationRoute);
+app.use("/request", verifyToken, RequestRoute);
 
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.send({
     status: err.status || 500,
-    message: err.message,
+    message: err.message
   });
 });
 
--- server/routes/request.js
+++ server/routes/request.js
@@ -1,55 +1,60 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const RequestHistory = require('../models/requestHistory');
-const UserVacation = require('../models/userVacation');
-const User = require('../models/User');
+const RequestHistory = require("../models/requestHistory");
+const UserVacation = require("../models/userVacation");
+const User = require("../models/User");
 const vacationType = require("../utils/vacationType");
 const useTypes = require("../utils/useTypes");
 const verifyToken = require("../middlewares/verifyToken");
 const calculateDateDiff = require("../utils/calculateDateDiff");
 const getUseDaysByUseType = require("../utils/getUseDaysByUseType");
+const { ObjectId } = require("mongoose").Types;
+
 
 // POST /request
-router.post('/:id', verifyToken, async (req, res) => {
+router.post("/:id", async (req, res) => {
   try {
     // 사용자가 입력한 정보
-    const {type, useType, startDate, endDate} = req.body;
+    const { type, useType, startDate, endDate } = req.body;
     const userId = req.params.id;
 
     // 휴가 사용자의 이름을 가져오기
     const user = await User.findById(userId);
     if (!user) {
-      return res.status(400).json({error: '사용자 정보를 찾을 수 없습니다.'});
+      return res.status(400).json({ error: "사용자 정보를 찾을 수 없습니다." });
     }
     const requestName = user.name;
 
     // 휴가 종류에 대한 유효성 검사
     if (!vacationType.includes(type)) {
-      return res.status(400).json({error: '잘못된 휴가유형입니다.'});
+      return res.status(400).json({ error: "잘못된 휴가유형입니다." });
     }
     // // 사용종류에 대한 유효성 검사
     if (!useTypes.includes(useType)) {
-      return res.status(400).json({error: '잘못된 사용종류입니다.'});
+      return res.status(400).json({ error: "잘못된 사용종류입니다." });
     }
-
 
     // 휴가 시작일자와 종료일자 검사
     const diffInDays = calculateDateDiff(startDate, endDate);
     if (diffInDays === -1) {
-      return res.status(400).json({error: '시작일자는 종료일자보다 이전이어야 합니다.'});
+      return res
+        .status(400)
+        .json({ error: "시작일자는 종료일자보다 이전이어야 합니다." });
     }
 
     // 사용자의 보유 휴가 정보 가져오기
-    const userVacation = await UserVacation.findOne({user: userId, type});
+    const userVacation = await UserVacation.findOne({ user: userId, type });
     if (!userVacation) {
-      return res.status(400).json({error: '해당 유형의 휴가 보유 기록을 찾을 수 없습니다.'});
+      return res
+        .status(400)
+        .json({ error: "해당 유형의 휴가 보유 기록을 찾을 수 없습니다." });
     }
 
     // 사용자의 잔여일 수 계산
     const requestDays = getUseDaysByUseType(useType) * diffInDays;
     if (userVacation.leftDays < requestDays) {
-      return res.status(400).json({error: '신청일이 잔여일을 초과합니다.'});
+      return res.status(400).json({ error: "신청일이 잔여일을 초과합니다." });
     }
 
     // RequestHistory 컬렉션에 새로운 요청 생성
@@ -60,8 +65,8 @@ router.post('/:id', verifyToken, async (req, res) => {
       useType,
       startDate,
       endDate,
-      status: 'pending',
-      requestDays
+      status: "pending",
+      requestDays,
     });
     await request.save();
 
@@ -69,19 +74,20 @@ router.post('/:id', verifyToken, async (req, res) => {
     userVacation.leftDays -= requestDays;
     await userVacation.save();
 
-    res.json({request, userVacation});
+    res.json({ request, userVacation });
   } catch (err) {
     console.error(err);
-    res.status(500).json({error: '서버 오류가 발생했습니다.'});
+    res.status(500).json({ error: "서버 오류가 발생했습니다." });
   }
 });
 
-router.get('/', verifyToken, async (req, res) => {
+router.get("/", async (req, res) => {
   const requestName = req.query.requestName; // requestName 검색 조건
   const type = req.query.type; // type 검색 조건
   const page = parseInt(req.query.page) || 1; // 페이지 번호, 기본값 1
   const limit = parseInt(req.query.limit) || 10; // 페이지당 문서 개수, 기본값 10
 
+  // TODO 검색기간 조회 넣기
   // 이 검색조건은 무엇을 위한 검색조건인가?
   // 생성일시?
   // 아니면 휴가 일시?
@@ -90,31 +96,47 @@ router.get('/', verifyToken, async (req, res) => {
 
   // requestName 검색 조건이 있는 경우
   if (requestName) {
-    conditions.requestName = new RegExp(requestName, 'i');
+    conditions.requestName = new RegExp(requestName, "i"); // requestName 검색 조건이 있다면, 대소문자 구분 없이 검색
   }
 
   // type 검색 조건이 있는 경우
   if (type) {
     if (type !== "") {
-      conditions.type = type;
+      conditions.type = type; // type 검색 조건이 있다면, 검색 조건 객체에 추가
     }
   }
   const options = {
-    skip: (page - 1) * limit,
-    limit: limit
+    skip: (page - 1) * limit, // 페이지 번호에 따라 건너뛸 문서 개수
+    limit: limit, // 페이지당 문서 개수
   };
 
-  const requests = await RequestHistory.find(conditions, null, options)
-  .exec();
-  const totalCount = await RequestHistory.countDocuments(conditions).exec();
+  const requests = await RequestHistory.find(conditions, null, options) // 검색 조건과 옵션에 따라 문서 검색
+    .exec();
+  const totalCount = await RequestHistory.countDocuments(conditions).exec(); // 검색 조건에 해당하는 문서 개수 조회
 
   res.json({
-    data: requests,
-    currentPage: page,
-    totalPages: Math.ceil(totalCount / limit),
-    totalDocuments: totalCount
+    data: requests, // 검색된 문서들
+    currentPage: page, // 현재 페이지 번호
+    totalPages: Math.ceil(totalCount / limit), // 전체 페이지 수
+    totalDocuments: totalCount, // 검색된 문서 개수
   });
+});
 
+router.get("/:id", async (req, res) => {
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      return res.status(400).json({ message: "Invalid id" });
+    }
+    const request = await RequestHistory.findById(req.params.id);
+
+    if (!request) {
+      return res.status(404).json({ message: "Request not found" });
+    }
+
+    res.status(200).json(request);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
 
 module.exports = router;
@@ -124,4 +146,3 @@ module.exports = router;
 // GET /request/:id 상세조회
 // PATCH /request/:id 수정
 // PATCH /request/:id/cancel 취소
-
