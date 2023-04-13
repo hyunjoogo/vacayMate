75879aad37661cee5a0fceb7a4869a57e76d0134
HyunJoo Kim <hyunjoogo@gmail.com>
2023-04-13 23:26:10 +0900

    B - admin 전용 URL 생성

--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -3,4 +3,36 @@ const router = express.Router();
 
 // admin
 
+// 회원정보 목록조회
+router.get('/members')
+
+// 회원정보 상세조회
+router.get('/members/:memberNo')
+
+// 회원정보 수정(부서, 직책)
+router.patch('/members/:memberNo')
+
+// 회원 관리자 등업
+router.patch('/members/:memberNo/role')
+
+// 회원 입사날짜 등록
+router.post('/members/:memberNo/enter-date')
+
+// 회원 퇴사처리
+router.patch('/members/:memberNo/out')
+
+
+// 회원 요청 목록조회
+router.get('/request')
+
+// 회원 요청 상세조회
+router.get('/request/:requestId')
+
+// 회원 요청 승인
+router.post('/request/:requestId/approve')
+
+// 회원 요청 거절
+router.post('/request/:requestId/refuse')
+
+
 module.exports = router;
--- server/routes/user/index.js
+++ server/routes/user/index.js
@@ -1,26 +1,22 @@
 const express = require('express');
+const router = express.Router();
 const profileController = require("../../controller/user/profileController");
 const vacationController = require("../../controller/user/vacationController");
 const requestController = require("../../controller/user/requestController");
 
-const router = express.Router();
-
 // 사용자 상세조회
-// getProfile
 router.get('/profile', profileController.getProfile)
 
 // 휴가종류 조회
-// getVacations
 router.get('/vacation', vacationController.getVacations);
 // 수정?
 router.patch('/vacation');
 
 // 휴가사용요청 조회
-// getRequests
 router.get('/request', requestController.getRequest);
 // 휴가사용요청 등록
-// createRequest
 router.post('/request', requestController.createRequest);
+
 // 휴가사용요청 상세조회
 // getDetailRequest
 router.get('/request/:requestId');
