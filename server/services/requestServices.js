ae71acd80e7ca71564c018fbf19d8f29c338fd35
HyunJoo Kim <hyunjoogo@gmail.com>
2023-04-15 19:27:46 +0900

    B - 코드정리

--- server/services/requestServices.js
+++ server/services/requestServices.js
@@ -2,10 +2,10 @@ const checkDuplicateUsingType = require("../functions/compareUsingType");
 const validationError = require("../exceptions/validation-error");
 const {Request} = require('../models/index');
 
-
+// 중복 요청이 있으면 false를 리턴하는 함수
 exports.checkDuplicateRequest = async (userId, vacationId, request) => {
   const {useDate, usingType} = request;
-  // 중복 휴가신청을 막는 코드
+
   // 같은 날짜의 요청 가지고 오기
   const sameUseDateRequests = await Request.findAll(
     {
@@ -15,7 +15,6 @@ exports.checkDuplicateRequest = async (userId, vacationId, request) => {
       }
     }
   );
-  // 없으면 바로 입력
   if (sameUseDateRequests.length !== 0) {
     // 신청한 사용타입과 겹치는지 확인
     const isPossible = checkDuplicateUsingType(sameUseDateRequests, usingType);
