481b979638f8842a4cc616e5efedb2c45f0ca3d3
HyunJoo Kim <hyunjoogo@gmail.com>
2023-04-16 19:38:29 +0900

    B - module로 변경 (import, export ) 가능

--- server/controller/user/requestController.js
+++ server/controller/user/requestController.js
@@ -72,8 +72,6 @@ const getDetailRequest = async (req, res) => {
 
   try {
     const request = await RequestServices.getDetailRequest(requestId);
-
-
     res.status(200).json(request);
 
   } catch (error) {
