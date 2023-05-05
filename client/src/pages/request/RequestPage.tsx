f8fe8fe07ba442d532d729d28085e941b7a376d9
HyunJoo Kim <hyunjoogo@gmail.com>
2023-05-06 03:19:14 +0900

    #36 요청 추가하기 UI 추가 및 기능 구현 - 공휴일 확인하는 함수 구현 후 기능 수정 필요

--- client/src/pages/request/RequestPage.tsx
+++ client/src/pages/request/RequestPage.tsx
@@ -23,8 +23,49 @@ const vacations: Vacation[] = [
   { type: "대체휴무", leftDays: 3, totalDays: 3, expirationDate: "2023-06-31" },
 ];
 
+const usingTypes = ["일차", "오전반차", "오후반차"];
+
 const RequestPage = () => {
   const [requests, setRequests] = useState<Request[]>([]);
+  const [selectedValue, setSelectedValue] = useState({
+    type: "",
+    usingType: "",
+    startDt: nowFormat("YYYY-MM-DD"),
+    endDt: nowFormat("YYYY-MM-DD"),
+  });
+
+  const addRequest = () => {
+    console.log(selectedValue);
+    setRequests([
+      ...requests,
+      {
+        ...selectedValue,
+        usingDays: getUsingDays(
+          selectedValue.usingType,
+          selectedValue.startDt,
+          selectedValue.endDt
+        ),
+      },
+    ]);
+  };
+
+  const getUsingDays = (usingType: string, startDt: string, endDt: string) => {
+    const diff = getDaysDiff(startDt, endDt);
+    const defaultUsingDaysByType = usingType === "일차" ? 1 : 0.5;
+    if (diff === 0) {
+      return defaultUsingDaysByType;
+    }
+    return defaultUsingDaysByType * diff;
+  };
+
+  // TODO 공휴일인지 확인하는 함수 구현 후 다시 진행할 것
+  // 토요일, 일요일, 공휴일이 들어가 있으면 날짜를 안쳐야한다
+  const getDaysDiff = (startDt: string, endDt: string): number => {
+    const start = dayjs(startDt);
+    const end = dayjs(endDt);
+    const diff = end.diff(start, "day");
+    return diff;
+  };
 
   return (
     <>
@@ -34,6 +75,65 @@ const RequestPage = () => {
             {vacation.type} : {vacation.leftDays} / {vacation.totalDays}
           </li>
         ))}
+      </ul>
+      <div>
+        <label>휴가 유형</label>
+        <select
+          value={selectedValue.type}
+          onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+            console.log(e.target.value);
+            setSelectedValue({ ...selectedValue, type: e.target.value });
+          }}
+        >
+          {vacations.map((vacation) => (
+            <option key={vacation.type} value={vacation.type}>
+              {vacation.type}
+            </option>
+          ))}
+        </select>
+        <label>사용 유형</label>
+        <select
+          value={selectedValue.usingType}
+          onChange={(e: React.ChangeEvent<HTMLSelectElement>) => {
+            setSelectedValue({ ...selectedValue, usingType: e.target.value });
+          }}
+        >
+          {usingTypes.map((type) => (
+            <option key={type} value={type}>
+              {type}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+        <label>시작날짜</label>
+        <input
+          type="date"
+          value={selectedValue.startDt}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setSelectedValue({ ...selectedValue, startDt: e.target.value });
+          }}
+        />
+        <label>종료일</label>
+        <input
+          type="date"
+          value={selectedValue.endDt}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setSelectedValue({ ...selectedValue, endDt: e.target.value });
+          }}
+        />
+        <button onClick={addRequest}>+</button>
+      </div>
+      <hr />
+      <ul>
+        {requests.map((request, index) => {
+          return (
+            <li key={index}>
+              {request.type} - {request.usingType} : {request.usingDays} <br />
+              {request.startDt} ~ {request.endDt}
+            </li>
+          );
+        })}
       </ul>
     </>
   );
