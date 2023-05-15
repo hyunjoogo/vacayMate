30b35667e0dc75b0eeb3d29ae81d881f1a34423e
HyunJoo Kim <hyunjoogo@gmail.com>
2023-05-16 08:29:37 +0900

    #36 B-회원의 Vacations의 키값 변경

--- server/services/vacationServices.js
+++ server/services/vacationServices.js
@@ -5,25 +5,42 @@ import { calculateTotalAnnual } from "../functions/calculateAnnual.js";
 const getUserAllVacations = async (userId) => {
   const userAllVacations = await db.Vacation.findAll({
     where: {
-      user_id: userId
-    }
+      user_id: 1,
+    },
   });
-  return userAllVacations;
+
+  const newUserAllVacations = userAllVacations.map((vacation) => {
+    return {
+      id: vacation.id,
+      type: vacation.type,
+      memo: vacation.memo,
+      leftDays: vacation.left_days,
+      totalDays: vacation.total_days,
+      expirationDate: vacation.expiration_date,
+      userId: vacation.user_id,
+    };
+  });
+
+  return newUserAllVacations;
 };
 
 // 하나의 휴가유형을 찾는다.
 const getUserVacationByPK = async (vacationId) => {
   const vacation = await db.Vacation.findByPk(vacationId, {
     include: {
-      model: db.User
-    }
+      model: db.User,
+    },
   });
   return vacation;
 };
 
-const createAnnualVacation = async (member, {enterDate, annualMemo}, transaction) => {
+const createAnnualVacation = async (
+  member,
+  { enterDate, annualMemo },
+  transaction
+) => {
   // 연차 계산 후 생성하기
-  const {expirationDate, totalAnnualDays} = calculateTotalAnnual(enterDate);
+  const { expirationDate, totalAnnualDays } = calculateTotalAnnual(enterDate);
 
   const memberAnnualVacation = await member.createVacation(
     {
@@ -31,10 +48,12 @@ const createAnnualVacation = async (member, {enterDate, annualMemo}, transaction
       memo: annualMemo, // 메모를 받아야겠네?
       left_days: totalAnnualDays,
       total_days: totalAnnualDays,
-      expiration_date: expirationDate
-    }, {transaction: transaction});
+      expiration_date: expirationDate,
+    },
+    { transaction: transaction }
+  );
 
   return memberAnnualVacation;
 };
 
-export { getUserAllVacations, getUserVacationByPK, createAnnualVacation};
+export { getUserAllVacations, getUserVacationByPK, createAnnualVacation };
